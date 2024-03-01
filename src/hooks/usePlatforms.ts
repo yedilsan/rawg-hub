687b58359473cfea4ea198503ed3ae52b29e6d45
yedilsan <yedilsanatovv@gmail.com>
2024-03-01 13:17:33 +0600

    Fetching Platforms

--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -1,11 +1,23 @@
+import { useQuery } from '@tanstack/react-query';
 import platforms from '../data/platforms';
-
+import apiClient from '../services/api-client';
+import { FetchResponse } from './useData';
+import { Platform } from './useGames';
 // interface Platform {
 // 	id: number;
 // 	name: string;
 // 	slug: string;
 // }
 
-const usePlatforms = () => ({ data: platforms, error: null });
+const usePlatforms = () =>
+	useQuery({
+		queryKey: ['platforms'],
+		queryFn: () =>
+			apiClient
+				.get<FetchResponse<Platform>>('/platform/list/parents')
+				.then(res => res.data),
+		staleTime: 24 * 60 * 60 * 1000, //24h
+		initialData: { count: platforms.length, results: platforms },
+	});
 
 export default usePlatforms;
