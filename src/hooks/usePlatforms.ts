fb573e76d57455f460f3e6163a8e6365e00c854b
yedilsan <yedilsanatovv@gmail.com>
2024-03-01 14:57:19 +0600

    Refactoring: create lookup hooks
    change data objects for platform and genre
    created hooks to find id for genres, platform

--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -2,7 +2,8 @@ import { useQuery } from '@tanstack/react-query';
 import platforms from '../data/platforms';
 import APIClient from '../services/api-client';
 
-const apiClient = new APIClient<Platform>('/platform/list/parents');
+const apiClient = new APIClient<Platform>('/platforms/lists/parents');
+
 export interface Platform {
 	id: number;
 	name: string;
@@ -14,7 +15,7 @@ const usePlatforms = () =>
 		queryKey: ['platforms'],
 		queryFn: apiClient.getAll,
 		staleTime: 24 * 60 * 60 * 1000, //24h
-		initialData: { count: platforms.length, results: platforms, next: null },
+		initialData: platforms,
 	});
 
 export default usePlatforms;
